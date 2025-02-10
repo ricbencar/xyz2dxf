9c621f5319746f074ac36826f8cb45428863dafc
Ricardo Carvalho <51909255+ricbencar@users.noreply.github.com>
2025-02-10 18:41:47 +0000

    Add files via upload

--- xyz2dxf_gui.cpp
+++ xyz2dxf_gui.cpp
@@ -8,6 +8,30 @@
  * - **Bicubic Spline (Default)**
  * - **Thin Plate Spline (TPS)**
  *
+ * **License:**
+ * -----------
+ * MIT License
+ *
+ * Copyright (c) 2025 XYZ to DXF Converter Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  * **Overview:**
  * The XYZ to DXF Converter GUI offers a streamlined workflow that allows users to:
  * - Select an input `.xyz` file using a standard Windows file dialog.
@@ -187,7 +211,7 @@
  *      configurations used, and key statistical data, serving as a valuable reference.
  *
  ******************************************************************************/
-
+ 
 #include <windows.h>
 #include <commdlg.h>
 #include <string>
